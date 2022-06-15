5d02b7bb5515093c03b76e5495318437d0865c48
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-15 13:23:33 +0300

    commit tree 19bb364379d17fc925a9744b6948ac325909c4d4

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function addingEventListener() {
     }
     input.addEventListener('click', clickAlert);
 }
+const input = document.getElementById('input');
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
